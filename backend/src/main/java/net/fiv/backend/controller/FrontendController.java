1273a3fd7cb380e0cc505a07472ea215333e8bc8
Fiv_ <nzaikar184@gmail.com>
2025-05-02 11:26:40 +0200

    change minecraft mod folder

--- backend/src/main/java/net/fiv/backend/controller/FrontendController.java
+++ backend/src/main/java/net/fiv/backend/controller/FrontendController.java
@@ -1,18 +1,25 @@
 package net.fiv.backend.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
+
+@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 @RequestMapping("/test")
 public class FrontendController {
 
-    @GetMapping
-    @ResponseBody
-    public String hello() {
-        return "<h1>Hello World</h1>";
+    @PostMapping("hello_world")
+    public ResponseEntity<String> hello(@RequestBody HashMap<String, String> message){
+        if (message.get("message").equals("server pidar")) {
+            System.out.println(message.get("message"));
+            return new ResponseEntity<>("sam pidar", HttpStatusCode.valueOf(400));
+        }
+        System.out.println(message.get("message"));
+        return new ResponseEntity<>("Message delivered", HttpStatusCode.valueOf(200));
     }
 
 }
---
+++ frontend/src/components/message.jsx
@@ -0,0 +1,59 @@
+import axios from "axios";
+import {useState} from "react";
+
+
+function Message() {
+
+    const uploadState = <div className={"bg-primary"}>Uploading...</div>
+    const succedState = <div className={"bg-success"}>Uploading good!</div>
+    const failedState = <div className={"bg-danger"}>Uploading failed!</div>
+
+    const [messageState, setState] = useState(-1)
+
+    function sendMessage(){
+        const doc = document.getElementById("exampleInputEmail1")
+        setState(0)
+        console.log(doc.value)
+        axios({
+            method: 'post',
+            url: 'http://localhost:8080/test/hello_world',
+            data: {
+                message: doc.value
+            }
+        }).then( (response) => {
+            console.log(response.data)
+            if(response.status === 200){
+                setState(200)
+            }
+        }).catch((error) => {
+            console.log(error.data)
+            if(error.status === 400){
+                setState(400)
+            }
+        });
+    }
+
+    return (
+        <>
+            <form>
+                <div className="mb-3">
+                    <label htmlFor="exampleInputEmail1" className="form-label">Send message to the server</label>
+                    <input type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+                </div>
+
+                <button type="button" className="btn btn-primary" onClick={sendMessage}>Submit</button>
+            </form>
+            {
+                messageState === 0 && uploadState
+            }
+            {
+                messageState === 200 && succedState
+            }
+            {
+                messageState === 400 && failedState
+            }
+        </>
+    )
+}
+
+export default Message
