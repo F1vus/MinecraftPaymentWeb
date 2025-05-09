ac13decc802af046f0e4892001af1717d89eadff
Fiv_ <nzaikar184@gmail.com>
2025-05-09 22:47:47 +0200

    Get information about minecraft server

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 import './App.css'
-import Header from "./components/Header.jsx";
 import Signup from "./pages/Signup.jsx"
 import { Routes, Route } from 'react-router-dom';
 import Home from "./pages/Home.jsx";
